10771ef8d18a1dd67a918036362e2737b322e04b
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-11-07 19:36:02 -0300

    add changes

--- atv07/atv07.R
+++ atv07/atv07.R
@@ -10,24 +10,27 @@ for (i in 1:5) {
   resultados_calculados[i] <- array_binomias[i] * 625
 }
 
-cal_qui_quadrado <- function(observado, esperado) {
-  
-  resultado_calculado <- exp((observado - esperado)) / esperado
-  
-  return resultado_calculado
-}
-
 soma_dos_resultados <- 0
 
-for (dado in 0:5) {
+for (i in 1:5) {
   
-  soma_dos_resultados <-  
-    
+  soma_dos_resultados <- soma_dos_resultados + array_binomias[i]  
+  
 }
 
-
-
-
-
+cal_qui_quadrado <- function(observado, esperado) {
   
+  resultado_calculado <- (exp((observado - esperado)) / esperado)
+  
+  return (resultado_calculado)
+}
 
+resultados_qui_quadrado <- c()
+
+array_freq_obervadas <- c(72, 204, 228, 101, 20)
+
+for( i in 1:5) {
+  
+  resultados_qui_quadrado[i] <-  cal_qui_quadrado(array_freq_obervadas[i],array_binomias[i])
+  
+}
