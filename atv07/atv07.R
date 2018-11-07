998e5df52c02d49110e7d98dd57f035bca79cb5f
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-11-07 20:23:59 -0300

    add changes

--- atv07/atv07.R
+++ atv07/atv07.R
@@ -1,26 +1,17 @@
 # Primeira questao
 
-array_binomias <- c(dbinom(0, 4, 2/5), dbinom(1, 4, 2/5), dbinom(2, 4, 2/5), dbinom(3, 4, 2/5), dbinom(4, 4, 2/5))
+nivel = 0.05
+
+array_freq_esperadas <- c(dbinom(0, 4, 2/5), dbinom(1, 4, 2/5), dbinom(2, 4, 2/5), dbinom(3, 4, 2/5), dbinom(4, 4, 2/5))
 
 resultados_calculados <- c()
 
 for (i in 1:5) {
-  resultados_calculados[i] <- array_binomias[i] * 625
-}
-
-soma_dos_resultados <- 0
-
-for (i in 1:5) {
-  
-  soma_dos_resultados <- soma_dos_resultados + array_binomias[i]  
-  
+  resultados_calculados[i] <- array_freq_esperadas[i] * 625
 }
 
 cal_qui_quadrado <- function(observado, esperado) {
-  
-  resultado_calculado <- (exp((observado - esperado)) / esperado)
-  
-  return (resultado_calculado)
+  return (((observado - esperado) ^ 2) / esperado)
 }
 
 resultados_qui_quadrado <- c()
@@ -29,6 +20,12 @@ array_freq_obervadas <- c(72, 204, 228, 101, 20)
 
 for( i in 1:5) {
   
-  resultados_qui_quadrado[i] <-  cal_qui_quadrado(array_freq_obervadas[i],array_binomias[i])
+  resultados_qui_quadrado[i] <-  cal_qui_quadrado(array_freq_obervadas[i],resultados_calculados[i])
   
+}
+
+soma_qui_quadrado_geral <- 0
+
+for (i in 1:5) {
+  soma_qui_quadrado_geral <- soma_qui_quadrado_geral + resultados_qui_quadrado[i]
 }
